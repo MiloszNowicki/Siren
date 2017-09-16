dc7e395fd4f7b12089eedfb4808a0a6fcf0de127
Milosz Nowicki <milosz.mily.nowicki@gmail.com>
2017-09-17 01:57:24 +0200

    implemented simple soluton for caruosel

--- src/js/utility.js
+++ src/js/utility.js
@@ -1,29 +1,49 @@
+/*
+$(document).ready(function () {
+
+    $('#third-carousel').carousel('pause');
+    $('#third-carousel').find('.item').each(function () {
+        var next_sibling = $(this).next();
+        if (!next_sibling.length) {
+            next_sibling = $(this).siblings(':first');
+        }
+        next_sibling.children(':first-child').find('img').clone().appendTo($(this).find('.image-flow'));
+
+        if (next_sibling.next().length > 0) {
+            next_sibling.next().children(':first-child').find('img').clone().appendTo($(this).find('.image-flow'));
+        }
+        else {
+            $(this).siblings(':first').children(':first-child').find('img').clone().appendTo($(this).find('.image-flow'));
+        }
+    });
+});
+
 
 /*$(function(){
   console.log($('#third-carousel'));
   $('#third-carousel').carousel();
+  $(document).ready(function () {
+
+      $('#third-carousel').carousel({
+          interval: 10000
+      })
+      $('#third-carousel').find('.item').each(function () {
+          var next_sibling = $(this).next();
+          if (!next_sibling.length) {
+              next_sibling = $(this).siblings(':first');
+              console.log('wtf');
+          }
+          next_sibling.children(':first-child').clone().appendTo($(this));
+
+          if (next_sibling.next().length > 0) {
+              next_sibling.next().children(':first-child').clone().appendTo($(this));
+              console.log('wtf2');
+          }
+          else {
+              $(this).siblings(':first').children(':first-child').clone().appendTo($(this));
+              console.log('wtf3');
+          }
+      });
+  });
 
 });*/
-$(document).ready(function () {
-
-    $('#third-carousel').carousel({
-        interval: 10000
-    })
-    $('#third-carousel').find('.item').each(function () {
-        var next = $(this).next();
-        if (!next.length) {
-            next = $(this).siblings(':first');
-            console.log('wtf');
-        }
-        next.children(':first-child').clone().appendTo($(this));
-
-        if (next.next().length > 0) {
-            next.next().children(':first-child').clone().appendTo($(this));
-            console.log('wtf2');
-        }
-        else {
-            $(this).siblings(':first').children(':first-child').clone().appendTo($(this));
-            console.log('wtf3');
-        }
-    });
-});
